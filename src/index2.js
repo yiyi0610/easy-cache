f9dbfaa39fa8bc2bee9b72cffd553564b6de8729
yiyi0610 <1054374938@qq.com>
2019-06-21 18:08:36 +0800

    图片上传

--- src/index2.js
+++ src/index2.js
@@ -17,15 +17,37 @@ window.onload = function(){
 
     function change(){
         let content = document.getElementById("upload");
+        let img = document.getElementById('img')
 
-        //content.onchange = function(e){ console.log("xxx",e); jsfunc.imageUpload(e);}
         content.addEventListener("change",function(e){
-            jsfunc.imageUpload(e);
+            jsfunc.imageUpload(e,setSrc);
         })
+
+    }
+
+    function setSrc(imgpath){
+        img.setAttribute('src',imgpath);
+    }
+
+
+    function drawImage(){
+        let myCanvas = doucment.getElementById("myCanvas");
+        if(myCanvas.getContext){
+            let ctx = myCanvas.getContext('2d');
+          //  ctx.
+            ctx.beginPath()
+            ctx.moveTo(0,0);
+            ctx.lineTo(0,50);
+            ctx.lineTo(50,0);
+            ctx.closePath();
+            
+        }
+
 
     }
     changeText();
     change();
+    drawImage();
 
 
 
